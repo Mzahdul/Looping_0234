74b55ec8f64b408747aa6e942294382d49fc03b9
Mzahdul <seeglas8@gmail.com>
2026-03-30 22:08:27 +0700

    first_tugas

---
+++ loop_234.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int x;
+
+    //perulangan dengan FOR 
+    //mencetak tulisan sebsanyak 5 kali
+    cout<<"perulangan FOR"<<endl;
+    for(int i=0; i<=5; i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+    cout<<endl;
+    //untuk mendapatkan urutan angka acak yang berbeda 
+    srand(time(0));
+
+    //perulangan  dengan WHILE
+    cout<<"perulangan WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"angka acak: "<<x<<endl;
+        x = rand()%10;
+    }
+}
