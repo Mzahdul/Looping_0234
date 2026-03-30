5eb3d89a527a18fcef9b19d8646bdd8c66150d3c
Mzahdul <seeglas8@gmail.com>
2026-03-30 23:03:40 +0700

    ke13_tugas

--- swithCase.cpp
+++ swithCase.cpp
@@ -16,3 +16,24 @@ void input (){
     cout<< "Masukan kode jurusan (1-3): ";
     cin>> kodejurusan;
 }
+
+//prosedur untuk pengecekan kondisi
+void pilihanjurusan() {
+    switch (kodejurusan) {
+        case 1:
+            namajurusan = "Teknologi Informasi";
+            statuskota = "tersedia (15 kursi)";
+            break;
+        case 2:
+            namajurusan = "Teknik Elektro";
+            statuskota = "kuota penuh";
+            break;
+        case 3:
+            namajurusan = "Sistem Informasi";
+            statuskota = "tersedia (5 kursi)";
+            break;
+        default:
+            namajurusan = "tidak diketahui";
+            statuskota = "Error: Kode Tidak Valid";
+    }
+}
