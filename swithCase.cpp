6005bc6da5225f94dff3773fa5ed168dcc70a873
Mzahdul <seeglas8@gmail.com>
2026-03-30 22:56:58 +0700

    ke12_tugas

--- swithCase.cpp
+++ swithCase.cpp
@@ -5,3 +5,14 @@ using namespace std;
 int kodejurusan;
 string namajurusan;
 string statuskota;
+
+//prosedur input 
+void input (){
+    cout<< "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+    cout<< "1.teknologi informasi" << endl;
+    cout<< "2. teknik elektro" << endl;
+    cout<< "3. sistem informasi" << endl;
+    cout<< "------------------------------" << endl;
+    cout<< "Masukan kode jurusan (1-3): ";
+    cin>> kodejurusan;
+}
