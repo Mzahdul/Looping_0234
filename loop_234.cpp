e043822b00e0a81eb1c63fb4d8a3a42812724269
Mzahdul <seeglas8@gmail.com>
2026-03-30 22:47:45 +0700

    ke10_tugas

--- loop_234.cpp
+++ loop_234.cpp
@@ -97,4 +97,13 @@ int main(){
     cin>>NiLM;                                       
     cout<<"Masukan Nilai Bahasa Inggris = ";
     cin>>niLB;
+
+    rerata = (niLB+NiLM)/2;
+
+    if ((rerata >= 60) && (niLB >= 70))
+        status = "Lulus";
+    else 
+        status = "Tidak Lulus";
+
+    cout<<"Status Kelulusan = "<<status<<" , dengan nilai rata-rata = "<<rerata<<endl;
 }
